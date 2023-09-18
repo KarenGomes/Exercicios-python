ad867e75d491ee48806c3652f74f96436a547144
KarenGomes <karenecris12015@gmail.com>
2023-09-18 11:50:37 -0300

    second commit

---
+++ 5.py
@@ -0,0 +1,8 @@
+#Faça um programa que leia um número inteiro e imprima seu antecessor e sucessor:
+
+numero = int(input('Digite um número: '))
+antecessor = numero -1
+sucessor = numero + 1
+print('Analisando o valor {}, seu sucessor é {} e antecessor é {}'.format(numero, sucessor, antecessor))
+
+#print('Analisando o valor {}, seu sucessor é {} e antecessor é {}'.format(numero, (numero + 1), (numero - 1)))
